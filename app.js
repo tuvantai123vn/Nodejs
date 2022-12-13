f10c9e9ce8b5f292e75a056a91e3bf12efd2dec0
tuvantai123vn <nguyenthanhtu.cdct@gmail.com>
2022-12-13 16:06:28 +0700

    lab4.1

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-const http = require('http');
+const express = require('express');
 
-const routes = require('./routes')
+const app = express();
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-console.log(routes.someText)
+app.use(express.urlencoded({extended: false}));
+app.use(adminRoutes);
+app.use(shopRoutes);
+app.use( (req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>')
+})
 
-const server =  http.createServer(routes.handler);
-server.listen(3000);
+
+app.listen(3000);
